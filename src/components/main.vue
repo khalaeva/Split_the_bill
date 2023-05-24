dd80633b70e1646d8557f30a91d84a00688d8514
khalaeva <halaevaanna@gmail.com>
2023-05-24 14:30:13 +0300

    style list

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="main">
         <div class="main-add_friend">
-            <button type="button" class="btn btn-light main-add_friend-btn">Добавить человека</button>
+            <button 
+                @click="addInput" 
+                type="button" 
+                class="btn btn-light main-add_friend-btn">
+                Добавить человека
+            </button>
         </div>
-        <ol class="list-group list-group-numbered">
-            <li class="list-group-item">A list item</li>
-            <li class="list-group-item">A list item</li>
-            <li class="list-group-item">A list item</li>
-        </ol>
+        <ul 
+            class="list-group"
+            v-for="i in counter">
+            <li class="list-group-item" style="margin-bottom: 10px">
+                <div class="input-group" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                    <input type="text" class="form-control">
+                    <button class="delete_btn">Удалить</button>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+import { useMainStore } from '../stores/MainStore'
+import { ref } from 'vue'
 
+
+const mainStore = useMainStore();
+const counter = ref(0)
+
+function addInput() {
+    counter.value++
+}
 </script>
 
 <style lang="scss" scoped>
@@ -28,9 +51,43 @@
     &-add_friend{
         display: grid;
         &-btn{
+            height: 50px;
+            background-color: #B3A394;
+            border: #B3A394;
+            color: #FFFFFF;
             border-radius: 10px;
-            margin: 10px auto 10px auto;
+            margin: 10px auto 20px auto;
+            &:active{
+                background-color: #FFFFFF;
+                border: #B3A394;
+                color: #B3A394;
+            }
         }
+    }
+}
+.bi-person-circle{
+    color: #B3A394;
+    margin: 5px;
+    margin-right: 15px;
+}
+.form-control{
+    border-color: #B3A394;
+    border-radius: 5px 0 0 5px !important;
+    &:focus{
+        border-color: #B3A394;
+        box-shadow: 0 0 0 0.1rem #B3A394;
+    }
+}
+.delete_btn{
+    background-color: #B3A394;
+    border: #B3A394;
+    color: #FFFFFF;
+    border-radius: 5px;
+    margin-left: 5px;
+    &:active{
+        background-color: #FFFFFF;
+        border: #B3A394;
+        color: #B3A394;
     }
 }
 </style>
--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -2,6 +2,12 @@ import {defineStore} from 'pinia'
 
 export const useMainStore = defineStore('MainStore', {
     state: () => ({
+        friends: []
+    }),
+    getters: {
+
+    },
+    actions: {
         
-    })
+    }
 })
