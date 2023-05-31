0674262a7a6bd00f64ffa2d87ed5210eab91f5e7
khalaeva <halaevaanna@gmail.com>
2023-05-31 15:03:07 +0300

    math result

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -2,10 +2,28 @@ import {defineStore} from 'pinia'
 
 export const useMainStore = defineStore('MainStore', {
     state: () => ({
-        friends: [],
-        products: [],
-        whoPay: [],
-        class: 'card array-friends-card'
+        friends: ['a', 'b', 'c'],
+        products: [
+            {
+                name: 'apple',
+                price: 10,
+                payPerson: 'a',
+                eatPersons: ['c', 'b']
+            },
+            {
+                name: 'orange',
+                price: 20,
+                payPerson: 'b',
+                eatPersons: ['a', 'c']
+            },
+            {
+                name: 'beer',
+                price: 40,
+                payPerson: 'c',
+                eatPersons: ['a', 'b']
+            }
+        ],
+        who: {}
     }),
     actions: {
         addInputFriend() {
@@ -25,7 +43,7 @@ export const useMainStore = defineStore('MainStore', {
         addInputProd() {
             this.products.push({
                 name: '',
-                price: '',
+                price: 0,
                 payPerson: this.friends[0],
                 eatPersons: [],
             })
@@ -40,5 +58,25 @@ export const useMainStore = defineStore('MainStore', {
         addAllEatPersons(index) {
             this.products[index].eatPersons.push(this.friends) 
         },
+
+        getResult(){
+            for (let i in this.friends) {
+                this.who[this.friends[i]] = {}
+            }
+            for (let i in this.products) {
+                const summ = this.products[i].price / this.products[i].eatPersons.length //делим полную сумму продукта на едаков
+                for (let j in this.products[i].eatPersons) {
+                    this.who[this.products[i].eatPersons[j]][this.products[i].payPerson] = summ //считаем для каждого друга сколько кому он должен
+                }
+            }
+            // for (let i in this.who){
+            //     for (let j in this.who) {
+            //         if (i === j) {
+            //             graph[i][j] = 0
+            //         }
+            //     }
+            // }
+            // console.log(this.who)
+        }
     }
 })
