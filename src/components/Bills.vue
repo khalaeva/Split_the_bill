00dfbafa2462a5d5742189c21348b01688467455
khalaeva <halaevaanna@gmail.com>
2023-05-31 15:03:24 +0300

    math result

--- src/components/Bills.vue
+++ src/components/Bills.vue
@@ -1,11 +1,92 @@
 <template>
-    <p>dfsd</p>
+    <div class="bills">
+        <h1 class="bills-text">
+            Результаты
+        </h1>
+        <div class="checkboxes">
+            <button class="checkboxes-btn">Кто - кому</button>
+            <button class="checkboxes-btn">Кому - кто</button>
+        </div>
+        <ul 
+            class="list-group"
+            v-for="(friend, index) in mainStore.friends.length">
+            <li class="list-group-item" style="margin-bottom: 10px">
+                <div class="input-group" >
+                    <img v-if="mainStore.friends[index]" class="image-person" :src="`https://icotar.com/initials/${mainStore.friends[index]}.svg?bg=B3A394`" alt="img" />
+                    <img v-else class="image-person" :src="'https://icotar.com/initials/~.svg?bg=B3A394'" alt="img" />
+                    <input type="text" class="form-control" v-model="mainStore.friends[index]">
+                    <button @click="mainStore.deleteInputFriend(index)" class="delete_btn">Удалить</button>
+                </div>
+            </li>
+        </ul>
+        <button
+            type="button" 
+            class="btn btn-light bills-add_friend-btn"
+            @click="mainStore.getResult()">
+            Далее   
+        </button>
+    </div>
 </template>
 
 <script setup>
+import { useMainStore } from '../stores/MainStore'
 
+const mainStore = useMainStore();
 </script>
 
 <style lang="scss" scoped>
+.checkboxes-btn{
+    width: 50%;
+    height: 35px;
+    color: #FFFFFF;
+    background-color: #B3A394;
+    border: 0;
+    border-radius: 10px;    
+    margin-bottom: 10px;
+}
+.bills {
+    padding: 20px;
+    border: 1px solid #CDD7D6;
+    border-radius: 10px;
+    background-color: #CDD7D6;
+    width: 70%;
+    min-width: 900px;
+    margin: auto;
+    margin-top: 50px;
+    &-text{
+        background-color: #FFFFFF;
+        border-radius: 10px;
+        color: #B3A394;
+        text-align: center;
+        padding-bottom: 10px;
+        margin-bottom: 10px;
+    }
+}
+.image-person{
+    height: 45px;
+    width: 45px;
+    border-radius: 100% !important;
+    margin-right: 15px;
+}
+.form-control{
+    border-color: #B3A394;
+    border-radius: 5px 0 0 5px !important;
+    &:focus{
+        border-color: #B3A394;
+        box-shadow: 0 0 0 0.1rem #B3A394;
+    }
+}
+.delete_btn{
+    background-color: #B3A394;
+    border: #B3A394;
+    color: #FFFFFF;
+    border-radius: 5px;
+    margin-left: 5px;
+    &:active{
+        background-color: #FFFFFF;
+        border: #B3A394;
+        color: #B3A394;
+    }
+}
 
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,7 +2,9 @@
     <div class="main">
         <div class="main-add_friend">
             <button 
-                @click="mainStore.addInputProd" 
+                @click="mainStore.addInputProd(); 
+                isActiveArray.push(Array(mainStore.friends.length).fill(false));
+                AllIsActive.push(false)" 
                 type="button" 
                 class="btn btn-light main-add_friend-btn">
                 Добавить продукт
@@ -10,7 +12,7 @@
         </div>
         <ul 
             class="list-group"
-            v-for="(product, index) in mainStore.products">
+            v-for="(product, indexProd) in mainStore.products">
             <li class="list-group-item" style="margin-bottom: 10px">
                 <div class="input-group mb-3" style="margin: 5px 0 15px 0 !important">
                     <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
@@ -18,14 +20,14 @@
                     </svg>
                     <input type="text" class="form-control input-name" placeholder="Название" v-model="product.name">
                     <input type="text" class="form-control" placeholder="Цена" v-model="product.price">
-                    <button @click="mainStore.deleteInputProd(index)" class="delete_btn">Удалить</button>
+                    <button @click="mainStore.deleteInputProd(indexProd)" class="delete_btn">Удалить</button>
                 </div>
                 <div class="select">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-wallet-fill" viewBox="0 0 16 16" preserveAspectRatio="none">
                         <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
                         <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
                     </svg>
-                    <select class="form-select form-control" v-model="mainStore.products[index].payPerson">
+                    <select class="form-select form-control" v-model="mainStore.products[indexProd].payPerson">
                         <option 
                             v-for="friend in mainStore.friends" 
                             :value="friend"
@@ -34,18 +36,28 @@
                         </option>
                     </select>
                 </div>
-                <div class="array-friends" >
-                    <div class="card array-friends-card">
+                <div class="array-friends">
+                    <div 
+                        :class="[AllIsActive[indexProd] ? 'card array-friends-card active' : 'card array-friends-card']" 
+                        @click="mainStore.addAllEatPersons(indexProd); 
+                        AllIsActive[indexProd] = !AllIsActive[indexProd];
+                        AllIsActive[indexProd] ? isActiveArray[indexProd] = isActiveArray[indexProd].map(item => item = true) : isActiveArray[indexProd] = isActiveArray[indexProd].map(item => item = false);
+                        console.log(isActiveArray[indexProd])">
                         <img class="card-img-top image-person" :src="`https://icotar.com/initials/∞.svg?bg=FFFFFF&fg=000000`" alt="Card image cap">
                         <div class="card-body array-friends-card-body">
-                          <span class="card-text">Все</span>
+                          <span :class="[AllIsActive[indexProd] ? 'card-text active-text' : 'card-text']">Все</span>
                         </div>
                     </div>
-                    <div class="card array-friends-card" v-for="(friend, index) in mainStore.friends" @click="mainStore.addEatPerson(friend, index)">
-                        <img v-if="!isActive" class="card-img-top image-person" :src="`https://icotar.com/initials/${mainStore.friends[index]}.svg?bg=FFFFFF&fg=000000`" alt="Card image cap">
-                        <img v-else class="card-img-top image-person" :src="`https://icotar.com/initials/${mainStore.friends[index]}.svg?bg=000000&fg=FFFFFF`" alt="Card image cap">
+                    <div 
+                        :class="[isActiveArray[indexProd][index] ? 'card array-friends-card active' : 'card array-friends-card']" 
+                        v-for="(friend, index) in mainStore.friends" 
+                        @click="mainStore.addEatPerson(friend, indexProd); 
+                        isActiveArray[indexProd][index] = !isActiveArray[indexProd][index]">
+                        <img class="card-img-top image-person" :src="`https://icotar.com/initials/${mainStore.friends[index]}.svg?bg=FFFFFF&fg=000000`" alt="Card image cap">
                         <div class="card-body array-friends-card-body">
-                          <span class="card-text">{{ friend }}</span>
+                          <span 
+                          :class="[isActiveArray[indexProd][index] ? 'card-text active-text' : 'card-text']"
+                          >{{ friend }}</span>
                         </div>
                     </div>
                 </div>
@@ -67,8 +79,8 @@ import { useMainStore } from '../stores/MainStore'
 import { ref } from 'vue';
 
 const mainStore = useMainStore();
-const isActive = ref(0)
-
+const isActiveArray = ref([]);
+const AllIsActive = ref([])
 </script>
 
 <style lang="scss" scoped>
@@ -126,6 +138,12 @@ const isActive = ref(0)
             padding: 0 5px 0 5px;
             margin: 0 20px 0 20px;
         }
+    }
+}
+.active{
+    background-color: #102542;
+    &-text{
+        color: #FFFFFF;
     }
 }
 .image-person{
